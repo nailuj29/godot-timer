413a37e24a396ca7794f3cb427213b1cb1a1d8ff
Nailuj <nailuj29gaming@gmail.com>
2024-05-19 16:14:40 -0400

    :bug: Clear time when opening time edit widget

--- timer.gd
+++ timer.gd
@@ -7,7 +7,7 @@ extends Control
 func _on_edit_time_button_pressed() -> void:
 	edit_time_button.visible = false
 	time_input.visible = true
-
+	time_input.time = "000000"
 
 func _on_time_input_done(time: int) -> void:
 	time_input.visible = false
