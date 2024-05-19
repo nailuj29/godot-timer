632c324441575ebb984d0fc9de24f1f610330eaa
Nailuj <nailuj29gaming@gmail.com>
2024-05-19 16:46:58 -0400

    :sparkles: Pause logic and decrement logic

--- timer.gd
+++ timer.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var edit_time_button: Button = %EditTimeButton
 @onready var time_input: TimeInput = %TimeInput
 @onready var time_label: Label = %TimeLabel
+@onready var pause_button: Button = %PauseButton
 
 var time: float = 0:
 	set(value):
@@ -16,14 +17,39 @@ var time: float = 0:
 			time_label.text = str(hours).lpad(2, "0") \
 				+ ":" + str(minutes).lpad(2, "0") + ":" \
 				+ str(seconds).lpad(2, "0")
+		else:
+			time = 0
+			time_label.text = "00:00:00"
+
+
+var paused := false:
+	set(value):
+		paused = value
+		if value:
+			pause_button.text = "Unpause"
+		else:
+			pause_button.text = "Pause"
+
+
+func  _process(delta: float) -> void:
+	if not paused:
+		time -= delta
 
 func _on_edit_time_button_pressed() -> void:
 	edit_time_button.visible = false
 	time_input.visible = true
+	pause_button.disabled = true
 	time_input.time = "000000"
+	paused = true
 
 func _on_time_input_done(time: int) -> void:
 	time_input.visible = false
 	edit_time_button.visible = true
+	pause_button.disabled = false
+	paused = false
 	if (time != -1):
 		self.time = time
+
+
+func _on_pause_button_pressed() -> void:
+	paused = not paused
